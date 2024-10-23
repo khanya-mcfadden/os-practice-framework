36ce3b17acfbae974c31709aa30d0ddae96cb177
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2024-10-23 08:59:18 +0100

    fixed routing issues by editing the folder naming conventions

--- main-app.py
+++ main-app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('test.html')
 
 app.run(debug=True)
