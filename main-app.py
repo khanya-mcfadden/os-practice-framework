3423193c9283676be0aa8fd3c260dc2ac689bbd1
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2024-10-23 08:56:22 +0100

    Update render_template in main-app.py to use index.html instead of test.html

--- main-app.py
+++ main-app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
-    return render_template('test.html')
+    return render_template('index.html')
 
 app.run(debug=True)
