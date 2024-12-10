da35ccbc07ddbb12ccd8a1adb1a23713920929b8
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2024-12-10 14:21:05 +0000

    Update app.run to bind to all interfaces

--- flaskr/main.py
+++ flaskr/main.py
@@ -579,4 +579,4 @@ def page_not_found(_):
     return render_template("404.html"), 404
 if __name__ == "__main__":
     initialize()
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
